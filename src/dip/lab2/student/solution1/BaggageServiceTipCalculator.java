ed09969cd3baab704c1e6f3b49c39257c19f7931
johnny <johnny@172.16.100.13>
2015-09-21 17:30:28 -0500

    dip lab 2 fixes

--- src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
+++ src/dip/lab2/student/solution1/BaggageServiceTipCalculator.java
@@ -10,14 +10,14 @@ package dip.lab2.student.solution1;
  * @author johnny
  */
 public class BaggageServiceTipCalculator implements TipCalculator{
-    private static final double MIN_BILL = 0.00;
-    private static final double MAX_BILL = 100.00;
-    private static final String BILL_ENTRY_ERR =
+    private final double MIN_BILL = 0.00;
+    private final double MAX_BILL = 100.00;
+    private final String BILL_ENTRY_ERR =
             "Error: bill must be between " + MIN_BILL + " and "
             + MAX_BILL;
-    private static final double GOOD_RATE = 0.20;
-    private static final double FAIR_RATE = 0.15;
-    private static final double POOR_RATE = 0.10;
+    private  final double GOOD_RATE = 0.20;
+    private  final double FAIR_RATE = 0.15;
+    private  final double POOR_RATE = 0.10;
 
     private double baseTipPerBag;
     private int bagCount;
